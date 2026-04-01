37ff0d73d204ed65f4407d5141f6301ad29083eb
defenfiver <defenfiver45@gmail.com>
2026-04-01 18:18:45 -0400

    Assignment 8 Solution 1 before

---
+++ Assignment 8/Solution 1/old.py
@@ -0,0 +1,42 @@
+import numpy
+from testInput import testInput
+import cProfile
+
+def blend():
+    # x = int(input())
+    x = 10000
+    nums, inputs = [], []
+    # for i in range(x):
+    #     tmp = input().split(" ")
+    #     inputs.append([tmp[0], int(tmp[1])])
+    tmpInput = testInput().split("\n")
+    for i in range(x):
+        tmp = tmpInput[i].split(" ")
+        inputs.append([tmp[0], int(tmp[1])])
+
+    for i in range(x):
+        num = inputs[i]
+        action = num[0]
+        item = num[1]
+        if action == "a":
+            nums.append(item)
+            med = numpy.median(nums)
+            imed = int(med)
+            if imed == med:
+                med = imed
+            print(med)
+        elif action == "r":
+            if item not in nums:
+                print("Wrong!")
+            else:
+                nums.remove(item)
+                if not nums:
+                    print("Wrong!")
+                else:
+                    med = numpy.median(nums)
+                    imed = int(med)
+                    if imed == med:
+                        med = imed
+                    print(med)
+
+cProfile.run('blend()')
